38a5b875bd84442aaf8b7a0e58868e5ff179cff3
Masayuki Ono <ono@ripplex.com>
2014-11-06 10:35:38 +0900

    fabric導入

--- iOS/HAYO/AppDelegate.swift
+++ iOS/HAYO/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import AVFoundation
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- iOS/HAYO/HAYO-Bridging-Header.h
+++ iOS/HAYO/HAYO-Bridging-Header.h
@@ -2,7 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <Crashlytics/Crashlytics.h>
 #import "ObjcHelper.h"
 #import <Parse/Parse.h>
 #import "MagicalRecord.h"
